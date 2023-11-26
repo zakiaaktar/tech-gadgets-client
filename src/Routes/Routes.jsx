a4147ed92f34b01226e73145f3481304da5943df
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-11-26 18:50:47 +0100

    login part done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
 
 
 
@@ -12,7 +13,11 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home></Home>
-            }
+            },
+            {
+                path: "login",
+                element: <Login></Login>
+              },
         ]
     }
 ]);
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import { FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+
+
+const Login = () => {
+  const [disabled, setDisabled] = useState(true);
+
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, [])
+
+
+  const handleLogin = event => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  }
+
+
+  const handleValidateCaptcha = (e) => {
+    const user_captcha_value = e.target.value;
+    //console.log(value);
+    if (validateCaptcha(user_captcha_value)) {
+        setDisabled(false);
+    }
+    else {
+        setDisabled(true)
+    }
+  }
+ 
+
+ 
+  
+
+  return (
+    <section className="container mx-auto lg:mt-12">
+      <div className="lg:flex items-center justify-center lg:gap-32">
+        <div className="bg-gradient-to-r from-[#FF9B50] to-[#F94C10] w-full py-20 lg:py-80 text-center text-white rounded-b-xl lg:rounded-3xl">
+          <h2 className="font-bold text-5xl mb-4">Tech Gadgets</h2>
+          <h2 className="font-semibold text-4xl">Login </h2>
+        </div>
+        <div className="w-11/12 lg:w-full bg-slate-100 rounded-lg drop-shadow-2xl mx-auto">
+          <form onSubmit={handleLogin} className="card-body lg:px-32 pt-12 lg:pt-32 pb-12">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold text-xl text-[#444]">
+                  Email
+                </span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Enter Your Email"
+                className="input rounded focus:border-[#13a0fe]"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-semibold text-xl text-[#444]">
+                  Password
+                </span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter Your Password"
+                className="input rounded focus:border-[#13a0fe]"
+                required
+              />
+            </div>
+
+            {/* chaptcha */}
+        <div className="form-control">
+          <label className="label">
+          <LoadCanvasTemplate />
+          </label>
+          <input onBlur={handleValidateCaptcha}  type="text" name="captcha" placeholder="Type the Captcha Above" className="input input-bordered" required />
+         </div>
+
+            <div className="form-control mt-6">
+              <input disabled={disabled}
+                className="btn btn-outline rounded font-bold text-xl text-white bg-gradient-to-r from-[#FF9B50] to-[#F94C10] border-0 hover:opacity-80"
+                type="submit"
+                value="Login"
+              />
+            </div>
+            <div className="mt-4 text-center">
+              <p className="font-medium text-lg mb-4">
+                New in Tech Gadgets ? Please{" "}
+                <Link to="/register">
+                  <span className="text-[#F94C10]">Register</span>
+                </Link>
+              </p>
+              <p className="font-medium text-lg">Or Sign In With</p>
+              <button className="btn btn-circle btn-outline mt-4 text-[#F94C10]">
+                <FaGoogle className="text-2xl"></FaGoogle>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
