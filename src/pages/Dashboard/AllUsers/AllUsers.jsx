b0826aa71a675b0fefd7722ce6f7edfeeb072ab3
Zakia Aktar <zakiaaktar@Zakias-MacBook-Pro.local>
2023-12-12 21:29:21 +0100

    make admin and delete users part done

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import { FaTrashAlt, FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 
@@ -14,8 +15,49 @@ const AllUsers = () => {
     })
 
 
+    const handleMakeAdmin = user =>{
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res =>{
+            console.log(res.data)
+            if(res.data.modifiedCount > 0){
+                refetch();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${user.name} is an Admin Now!`,
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+            }
+        })
+    }
+
+
     const handleDeleteUser = user => {
-        
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "User has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
     }
 
 
@@ -47,7 +89,12 @@ const AllUsers = () => {
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>
-                        
+                    { user.role === 'admin' ? 'Admin' : <button
+                        onClick={() => handleMakeAdmin(user)}
+                        className="btn btn-lg bg-orange-500">
+                        <FaUsers className="text-white 
+                        text-2xl"></FaUsers>
+                        </button>}
                     </td>
                     <td>
                     <button
